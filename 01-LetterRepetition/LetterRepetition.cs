475566ced959becf5a86505189878731fab1136b
bayangarkov <baiangarkov@gmail.com>
2017-03-05 16:17:11 +0200

    Program witch reads every char in given string and shows how many times occurred

---
+++ 01-LetterRepetition/LetterRepetition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LetterRepetition
+{
+    class LetterRepetition
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            var result = new Dictionary<char, int>();
+
+            foreach (var str in inputString)
+            {
+                if (!result.ContainsKey(str))
+                {
+                    result[str] = 0;
+                }
+
+                result[str]++;
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
