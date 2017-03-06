9ca5ab244caa50e9536ec50cc7b97df2f986c50e
bayangarkov <baiangarkov@gmail.com>
2017-03-06 20:47:42 +0200

    Program witch add name and phone number and uses try parse method

---
+++ 03-MixedPhones/MixedPhones.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MixedPhones
+{
+    class MixedPhones
+    {
+        static void Main()
+        {
+            var inputStr = Console.ReadLine();
+
+            var dict = new SortedDictionary<string, decimal>();
+            
+            
+            while (inputStr != "Over")
+            {
+                var getFirst = inputStr.Split(' ')[0];
+                var getSecond = inputStr.Split(' ')[2];
+
+                decimal number;
+            
+                if (decimal.TryParse(getFirst, out number))
+                {
+                    dict.Add(getSecond,number);
+                }
+                else
+                {
+                    dict.Add(getFirst,Convert.ToDecimal(getSecond));
+                }
+            
+                inputStr = Console.ReadLine();
+            
+            }
+
+            foreach (var pair in dict)
+            {
+                var key = pair.Key;
+                var val = pair.Value;
+
+                Console.WriteLine(key + " -> " + val);
+            }
+
+        }
+    }
+}
