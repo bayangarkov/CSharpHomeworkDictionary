a029d6cda998c8764fd3f4948dd2e95298a60718
bayangarkov <baiangarkov@gmail.com>
2017-03-12 18:03:14 +0200

    Program witch inserting in dictionary hashset of ints

---
+++ 08-Shellbound/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Shellbound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allRegions = new Dictionary<string, HashSet<int>>();
+
+            var inputData = Console.ReadLine();
+
+            while (inputData != "Aggregate")
+            {
+                var getRegion = inputData.Split(' ')[0];
+                var getShells = Convert.ToInt32(inputData.Split(' ')[1]);
+
+                if (!allRegions.ContainsKey(getRegion))
+                {
+                    allRegions[getRegion] = new HashSet<int>();
+                }
+                allRegions[getRegion].Add(getShells);
+                
+
+                inputData = Console.ReadLine();
+
+            }
+
+            foreach (var pars in allRegions)
+            {
+                var key = pars.Key;
+                var val = pars.Value;
+                var sumOfShells = val.Sum();
+                var averageSum = val.Average();
+                var bigShells = sumOfShells - averageSum;
+
+                Console.WriteLine($"{key} -> {String.Join(", ", val)} ({bigShells:f0})");
+            }
+        }
+    }
+}
